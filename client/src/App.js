742a0e702eeec4209c11cc76f1cde165f2b81131
Tarosism <yar0606@naver.com>
2021-12-17 13:47:39 +0900

    fix: CRUD berg fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import GlobalStyle from './globalStyles';
-import Home from './pages/HomePage/Home';
-import SignUp from './pages/SignUp/SignUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop';
-import { Navbar, Footer } from './components';
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import Home from "./pages/HomePage/Home";
+import SignUp from "./pages/SignUp/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import { Navbar, Footer } from "./components";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
       <ScrollToTop />
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/sign-up' component={SignUp} />
+        <Route path="/" exact component={Home} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
       <Footer />
     </Router>
