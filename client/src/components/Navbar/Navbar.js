c06e37af94121928df9bc72735d1229bed77222e
yunjaekim <yunjaekim007@gmail.com>
2021-12-21 20:25:38 +0900

    feat : dropdown menu & login navbar
    login하면 Navbar 메뉴 변경 + 드랍다운 메뉴

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -12,25 +12,42 @@ import {
   NavLinks,
   NavBtnLink,
 } from "./Navbar.elements";
+import {
+  MapButtonLink,
+  SimpleDiv,
+  MapIcon,
+  InputText,
+  MainDropDown,
+} from "./Dropdown.elements";
 import Modal from "../Forms/Modal";
+import DropDownMenu from "./DropdownMenu";
+import { regionData } from "./RegionDummyData";
 
-const Navbar = ({ handleLoginSuccess, handleSignupSuccess }) => {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+const Navbar = ({
+  handleLoginSuccess,
+  handleSignupSuccess,
+  isLogin,
+  setIsLogin,
+}) => {
+  // const [click, setClick] = useState(false);
+  // const [button, setButton] = useState(true);
 
-  const handleClick = () => setClick(!click);
+  // const handleClick = () => setClick(!click);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+  // const showButton = () => {
+  //   if (window.innerWidth <= 960) {
+  //     setButton(false);
+  //   } else {
+  //     setButton(true);
+  //   }
+  // };
 
-  useEffect(() => {
-    showButton();
-  }, []);
+  // useEffect(() => {
+  //   showButton();
+  // }, []);
+
+  // 지역 데이터
+  const [currLoc, setCurrLoc] = useState("신논현역");
 
   // Modal bar 구현 부분
   const [showModal, setShowModal] = useState(false);
@@ -44,31 +61,82 @@ const Navbar = ({ handleLoginSuccess, handleSignupSuccess }) => {
     setShowModal((showModal) => !showModal);
   };
 
+  // 로그인 때 보이는 JSX를 변수에 넣기
+  // 로그인 상태 아닐 때 보이는 우측 버튼 두 개
+  const notLoginButton = (
+    <>
+      <NavItem>
+        <NavLinks>
+          <Button onClick={openLogin}>로그인</Button>
+        </NavLinks>
+      </NavItem>
+
+      <NavItemBtn>
+        <NavBtnLink>
+          <Button primary onClick={openSignup}>
+            회원가입
+          </Button>
+        </NavBtnLink>
+      </NavItemBtn>
+    </>
+  );
+
+  // dropdown bar
+  const dropDownBar = (
+    <>
+      <MapButtonLink>
+        <SimpleDiv>
+          <MapIcon />
+        </SimpleDiv>
+        <SimpleDiv>
+          <InputText value={currLoc} disabled />
+        </SimpleDiv>
+        <MainDropDown id="dropbox">
+          <DropDownMenu
+            regionData={regionData}
+            currLoc={currLoc}
+            setCurrLoc={setCurrLoc}
+          />
+        </MainDropDown>
+      </MapButtonLink>
+    </>
+  );
+
+  // 로그인되면 보일 우측 버튼 2개
+  const loginButton = (
+    <>
+      <NavItem>
+        <NavLinks>
+          <Button primary onClick={openLogin}>
+            My Page
+          </Button>
+        </NavLinks>
+      </NavItem>
+
+      <NavItemBtn>
+        <NavBtnLink>
+          <Button onClick={openSignup}>Log out</Button>
+        </NavBtnLink>
+      </NavItemBtn>
+    </>
+  );
 
   return (
     <>
       <IconContext.Provider value={{ color: "#472d0c" }}>
         <Nav>
           <NavbarContainer>
-            <NavLogo to="/main">
-              <NavIcon />
-              Cafe In 
-            </NavLogo>
-            <NavMenu onClick={handleClick} click={click}>
-              <NavItem>
-                <NavLinks to="/">
-                  <Button onClick={openLogin}>로그인</Button>
-                </NavLinks>
-              </NavItem>
-              <NavItemBtn>
-                <NavBtnLink to="/sign-up">
-                  <Button primary onClick={openSignup}>
-                    회원가입
-                  </Button>
-                </NavBtnLink>
-              </NavItemBtn>
-            </NavMenu>
+            <SimpleDiv>
+              <NavLogo to="/main">
+                <NavIcon />
+                Cafe In
+              </NavLogo>
+              {isLogin ? dropDownBar : null}
+            </SimpleDiv>
+
+            <NavMenu>{isLogin ? loginButton : notLoginButton}</NavMenu>
           </NavbarContainer>
+
           <Modal
             clickedMenu={clickedMenu}
             showModal={showModal}
@@ -80,6 +148,6 @@ const Navbar = ({ handleLoginSuccess, handleSignupSuccess }) => {
       </IconContext.Provider>
     </>
   );
-}
+};
 
 export default Navbar;
