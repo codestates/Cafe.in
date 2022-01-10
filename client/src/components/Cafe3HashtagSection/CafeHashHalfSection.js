f9b02ee46d75146b56f9876462cd9e6f789dbac8
yunjaekim <yunjaekim007@gmail.com>
2022-01-10 14:48:52 +0900

    feat : hashtag section UI
    Hashtag Section UI 추가

---
+++ client/src/components/Cafe3HashtagSection/CafeHashHalfSection.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import * as S from "./CafeHashtagMain.styled";
+import { Button } from "../../assets/styles/GlobalStyle";
+import HashtagInputSection from "./HashtagInputSection";
+import { options } from "./DummyDataHashtag";
+import heartIcon from "../../assets/images/heart-black-empty.svg";
+
+const HashtagHalfSection = ({ type, titleImg, hashtagBg, hashtagArray }) => {
+  // 해시태그 list wrapper에 쓸 className 등등 state들
+  const [divSize, setDivSize] = useState("short-div");
+  const [showMoreText, setShowMoreText] = useState("해시태그 더보기 >");
+  const [sliceIndex, setSliceIndex] = useState(3);
+
+  // Dropdown에 쓸 State
+  const [selected, setSelected] = useState({
+    [type]: "",
+  });
+
+  // Input Section의 Input text 값
+  const [inputText, setInputText] = useState({
+    [type]: "",
+  });
+  // console.log("category", selected, "inputText", inputText);
+
+  const handleFinalSubmit = () => {
+    //! submit 버튼 누르면 처리할 것. 코드 추가 예정
+    // let concatHashtag = selected[type] + " " + inputText[type];
+    //! select[type]은 obj.category, inputText[type]은 obj.name으로 서버로 보내준다.
+  };
+
+  // 해시태그 길이가 3보다 기냐?
+  let hashtagsLength = hashtagArray.length;
+  let isLongerThan3 = hashtagsLength > 3;
+
+  // 해시태그 List rendering
+  const hashtagListMap = hashtagArray
+    .slice(0, sliceIndex)
+    .map((hashtagElement, index) => {
+      return (
+        <S.CafeRank key={index}>
+          {index + 1}.{" "}
+          <S.CafeRankBox bgColor={hashtagBg}>
+            {hashtagElement.category + " " + hashtagElement.name}
+          </S.CafeRankBox>
+          <S.CafeRankCounts>{hashtagElement.counts}</S.CafeRankCounts>
+          <img src={heartIcon} alt="heart-icon" />
+        </S.CafeRank>
+      );
+    });
+
+  // Section Wrapper className 바꾸기용 + 텍스트 변경용
+  const makeDivLonger = () => {
+    if (divSize === "short-div") {
+      setDivSize("long-div");
+      setShowMoreText("< 다시 Top3만 보기");
+      setSliceIndex(hashtagsLength);
+    } else {
+      setDivSize("short-div");
+      setShowMoreText("해시태그 더보기 >");
+      setSliceIndex(3);
+    }
+  };
+
+  return (
+    <S.CafeHashtagSectionWrapper>
+      <S.CafeImg src={titleImg} />
+
+      <S.CafemoreWrapper onClick={makeDivLonger}>
+        {isLongerThan3 ? (
+          <S.Cafemore>{showMoreText}</S.Cafemore>
+        ) : (
+          <S.Cafemore>&nbsp;</S.Cafemore>
+        )}
+      </S.CafemoreWrapper>
+
+      <S.CafeListWrapper className={divSize}>
+        {hashtagListMap}
+      </S.CafeListWrapper>
+
+      <S.HashtagInputWrapper>
+        <HashtagInputSection
+          type={type}
+          selected={selected}
+          setSelected={setSelected}
+          options={options}
+          inputText={inputText}
+          setInputText={setInputText}
+          handleFinalSubmit={handleFinalSubmit}
+        />
+      </S.HashtagInputWrapper>
+    </S.CafeHashtagSectionWrapper>
+  );
+};
+
+export default HashtagHalfSection;
--- client/src/components/Cafe3HashtagSection/CafeHashtagMain.styled.js
+++ client/src/components/Cafe3HashtagSection/CafeHashtagMain.styled.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const CafeHashtagContainer = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center ;
+  align-items: center;
   width: 100%;
-
 
   @media screen and (max-width: 960px) {
     display: flex;
@@ -15,40 +14,23 @@ export const CafeHashtagContainer = styled.div`
     align-items: center;
     width: 100%;
   }
-`
-
-export const CafeHashtagLikeWrapper  = styled.div`
-height: 490px;
-width: 49%;
-background: ${ ({theme}) => theme.colors.mainBackground };
-border-radius:40px;
-box-shadow: 0 0px 10px #aaaaaa;
-align-items:center;
-text-align:center;
-display: center; 
-margin: 2px auto;
-margin-bottom:2rem;
-margin-top : 2rem;
-
-@media screen and (max-width: 960px) {
-    width: 100%;
-    &:hover {
-      transform: none;}
-    }
 `;
 
+// CafeHashtagLikeWrapper와 CafeHashtagHateWrapper 복사 붙이기 + 이름 바꾸기
+export const CafeHashtagSectionWrapper = styled.div`
+  height: 100%;
+  width: 49%;
+  background: ${({ theme }) => theme.colors.mainBackground};
+  border-radius: 40px;
+  box-shadow: 0 0px 10px #aaaaaa;
+  align-items: center;
+  text-align: center;
+  display: center;
+  margin: 2px auto;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
 
-export const CafeHashtagHateWrapper  = styled.div`
-height: 490px;
-width: 49%;
-border-radius:40px;
-box-shadow: 0 0px 10px #aaaaaa;
-align-items:center;
-text-align:center;
-display: center; 
-margin: 2px auto;
-
-@media screen and (max-width: 960px) {
+  @media screen and (max-width: 960px) {
     width: 100%;
     &:hover {
       transform: none;
@@ -56,168 +38,254 @@ margin: 2px auto;
   }
 `;
 
- 
-export const CafeLikeImg  = styled.img`
-  width:19%;
-  height:42%;
-  position: relative;
-  align-items:center;
-  text-align:center ;
-  margin-top: -1.3rem;
+// export const CafeHashtagLikeWrapper  = styled.div`
+// height: 490px;
+// width: 49%;
+// background: ${ ({theme}) => theme.colors.mainBackground };
+// border-radius:40px;
+// box-shadow: 0 0px 10px #aaaaaa;
+// align-items:center;
+// text-align:center;
+// display: center;
+// margin: 2px auto;
+// margin-bottom:2rem;
+// margin-top : 2rem;
 
+// @media screen and (max-width: 960px) {
+//     width: 100%;
+//     &:hover {
+//       transform: none;}
+//     }
+// `;
+
+// export const CafeHashtagHateWrapper  = styled.div`
+// height: 490px;
+// width: 49%;
+// border-radius:40px;
+// box-shadow: 0 0px 10px #aaaaaa;
+// align-items:center;
+// text-align:center;
+// display: center;
+// margin: 2px auto;
+
+// @media screen and (max-width: 960px) {
+//     width: 100%;
+//     &:hover {
+//       transform: none;
+//     }
+//   }
+// `;
+
+export const CafeImg = styled.img`
+//! border 임시
+border: 0.1rem solid green;
+  width: 15vh;
+  height: 20vh;
+  position: relative;
+  align-items: center;
+  text-align: center;
+  margin-top: -1.3rem;
+  margin-bottom: -1.3rem;
 
   @media screen and (max-width: 960px) {
-   width:30%;
-   height:27%;
+    width: 30%;
+    height: 27%;
   }
 `;
 
 
+// export const CafeLikeImg = styled.img`
+//   width: 19%;
+//   height: 42%;
+//   position: relative;
+//   align-items: center;
+//   text-align: center;
+//   margin-top: -1.3rem;
 
-export const CafeBadImg = styled.img`
-  width:18%;
-  height:40%;
-  position: relative;
-  align-items:center;
-  text-align:center ;
-  margin-top: -1.3rem;
-  
+//   @media screen and (max-width: 960px) {
+//     width: 30%;
+//     height: 27%;
+//   }
+// `;
 
-@media screen and (max-width: 960px) {
-  width:30%;
-   height:27%;
+// export const CafeBadImg = styled.img`
+//   width: 18%;
+//   height: 40%;
+//   position: relative;
+//   align-items: center;
+//   text-align: center;
+//   margin-top: -1.3rem;
+
+//   @media screen and (max-width: 960px) {
+//     width: 30%;
+//     height: 27%;
+//   }
+// `;
+
+export const CafeListWrapper = styled.div`
+//! border 임시
+  border: 0.1rem solid red;
+  &.short-div {
+    height: 40%;
   }
+  &.long-div {
+    min-height: 40%;
+    max-height: 30vh;
+    overflow: auto;
+  }
+`
 
-`;
-
-export const Cafemore = styled(Link)`
-
+export const Cafemore = styled.div`
   color: #aaaaaa;
-  text-shadow : 0px 1px 1px #CCCCCC;
+  text-shadow: 0px 1px 1px #cccccc;
 `;
 
+export const CafemoreWrapper = styled.div`
+//! border 임시
+  border: 0.1rem solid blue;
+  align-items: right;
+  text-align: right;
+  padding-right: 2rem;
+  margin-top: 0.4rem;
 
-export const CafemoreWrapper  = styled.div`
-
-align-items:right;
-text-align:right;
-padding-right:2rem;
-margin-top:.4rem;
-
-@media screen and (max-width: 1440px) {
-    width: 100%;}
+  @media screen and (max-width: 1440px) {
+    width: 100%;
+  }
 `;
 
+export const CafeRank = styled.div`
+  padding-top: -10px;
+  font-weight: 700;
+  font-size: 32px;
+  white-space: nowrap;
+  margin-top: -8px;
+  color: ${({ theme }) => theme.colors.cardBackground};
+  margin: 4px 20px 0px 10px;
+  text-align: left;
+  text-shadow: 0px 1px #aaaaaa;
+  display: flex;
+  flex-direction: row;
 
-export const CafeRank  = styled.p`
- padding-top:-10px;
- font-weight :700;
- font-size: 32px;
- white-space:nowrap;
- margin-top:-8px;
- color: ${ ({theme}) => theme.colors.cardBackground };
- margin : 4px 240px 0px 180px;
- text-align:left;
- text-shadow: 0px 1px #aaaaaa;
-
-
-@media screen and (max-width: 1440px) {
+  @media screen and (max-width: 1440px) {
     font-size: 30px;
     width: 100%;
-    margin : 4px 0px 0px 40px;
-    text-align:left;
-  
+    margin: 4px 0px 0px 40px;
+    text-align: left;
   }
-
 `;
 
+export const CafeRankCounts = styled.div`
+  font-size: 28px;
+  color: grey;
+  margin: 0 30px 0 30px;
+`
 
-export const　CafeRankBox  = styled.span`
-  font-weight :700;
-  width: 100%;
-  color:#fff;
+export const CafeRankBox = styled.span`
+  font-weight: 700;
+  width: 40%;
+  color: #fff;
   padding: 4px 10px 4px 10px;
-  vertical-align: middle; 
+  vertical-align: middle;
   line-height: 2;
   font-size: 17px;
-  background: #F2ACAC;
-  border-radius: 12px;  
+  background: ${({ bgColor }) => bgColor};
+  border-radius: 12px;
   box-shadow: 1px 1px #cccc;
-
-
-  ` 
-
-
-export const　CafeRankBox2  = styled.span`
-font-weight :700;
-color:#fff;
-padding: 4px 10px 4px 10px;
-vertical-align: middle; 
-line-height: 2;
-font-size: 17px;
-background: #77B9F2;
-border-radius: 12px;  
-box-shadow: 1px 1px #cccc;
-` 
-
-
-export const CafeInputWrapper = styled.div`
-align-items:center;
-text-align:center;
-white-space:nowrap;
-margin : 4px 200px 0px 180px;
-display: flex;
-background: #fff;
-padding-right:2rem;
-margin-top:2.7rem;
-
-@media screen and (max-width: 1440px) {
-    width: 10%;
-    text-align:center;
-    margin : 80px 0px 0px 10px;
-  
-  }
-
 `;
 
+// export const CafeRankBox  = styled.span`
+//   font-weight :700;
+//   width: 100%;
+//   color:#fff;
+//   padding: 4px 10px 4px 10px;
+//   vertical-align: middle;
+//   line-height: 2;
+//   font-size: 17px;
+//   background: #F2ACAC;
+//   border-radius: 12px;
+//   box-shadow: 1px 1px #cccc;
+//   `
 
+// export const CafeRankBox2  = styled.span`
+// font-weight :700;
+// color:#fff;
+// padding: 4px 10px 4px 10px;
+// vertical-align: middle;
+// line-height: 2;
+// font-size: 17px;
+// background: #77B9F2;
+// border-radius: 12px;
+// box-shadow: 1px 1px #cccc;
+// `
 
+export const CafeInputWrapper = styled.div`
+  border: 0.1rem solid red;
+  align-items: center;
+  text-align: center;
+  white-space: nowrap;
+  margin: 4px 10px 0px 10px;
+  display: flex;
+  background: #fff;
+  padding-right: 2rem;
+  margin-top: 2.7rem;
 
-
+  @media screen and (max-width: 1440px) {
+    width: 10%;
+    text-align: center;
+    margin: 80px 0px 0px 10px;
+  }
+`;
 
 export const Input = styled.input`
-height: 43%;
-font-weight: 600;
-text-align:center;
-color: ${"palevioletred"};
-background: ${ ({theme}) => theme.colors.mainBackground };
-opacity: .9;
-box-shadow: 1px #cccccc;
-border: 10px solid ${ ({theme}) => theme.colors.cardBackground };
-outline: none;
+  height: 2.5rem;
+  font-weight: 600;
+  /* text-align: center; */
+  color: ${"palevioletred"};
+  background: ${({ theme }) => theme.colors.mainBackground};
+  opacity: 0.9;
+  box-shadow: 1px #cccccc;
+  border: 10px solid ${({ theme }) => theme.colors.cardBackground};
+  outline: none;
 
-
-@media screen and (max-width: 1440px) {
+  @media screen and (max-width: 1440px) {
     width: 70%;
-    text-align:center; 
+    text-align: center;
   }
 `;
 
 export const Input2 = styled.input`
-height: 43%;
-font-weight: 600;
-text-align:center;
-color: ${ ({theme}) => theme.colors.buttonSecondary};
-background: ${ ({theme}) => theme.colors.mainBackground };
-opacity: .9;
-box-shadow: 1px #cccccc;
-border: 10px solid ${ ({theme}) => theme.colors.cardBackground };
-outline: none;
+  height: 43%;
+  font-weight: 600;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.buttonSecondary};
+  background: ${({ theme }) => theme.colors.mainBackground};
+  opacity: 0.9;
+  box-shadow: 1px #cccccc;
+  border: 10px solid ${({ theme }) => theme.colors.cardBackground};
+  outline: none;
 
-
-@media screen and (max-width: 1440px) {
+  @media screen and (max-width: 1440px) {
     width: 70%;
-    text-align:center; 
+    text-align: center;
   }
 `;
+
+export const HashtagInputWrapper = styled.div`
+//! border 임시
+  border: 0.1rem solid purple;
+  display: flex;
+  flex-direction: row;
+`
+
+export const DropdownMenu = styled.select`
+  width: 10vw;
+  height: 4vh;
+  border-radius: 1em;
+  text-align: center;
+  font-size: 1.1rem;
+  background-color: ${({ theme }) => theme.colors.mainBackground};
+  margin: 1rem;
+`
+
+
 
