6b507affccea5a34112f7d9dbbd817ed647ac8f6
yunjaekim <yunjaekim007@gmail.com>
2022-01-17 17:33:37 +0900

    버그 : Thunk 버그 수정
    Thunk 버그 수정

--- client/src/pages/CafeInfo/CafeInfo.js
+++ client/src/pages/CafeInfo/CafeInfo.js
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import * as S from "./CafeInfo.styled";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  login,
-  showModal,
-  loginUserInfo,
-  getCafeInfo,
-} from "../../store/actions";
-import { useNavigate } from "react-router-dom";
-import { postCountResetAction } from "../../store/actions";
-
+import { getCafeInfo } from "../../store/actions";
 import { Cafe1ImageSection } from "../../components";
 import { Cafe2InfoSection } from "../../components";
 import { Cafe3HashtagSection } from "../../components";
@@ -18,35 +10,20 @@ import { Cafe4MapSection } from "../../components";
 
 const CafeInfo = () => {
   const { id } = useParams();
-
   const isLogin = useSelector((state) => state.isLogin.isLogin);
-  const cafeInfo = useSelector((state) => state.cafeInfoReducer.data);
   const isLoading = useSelector((state) => state.cafeInfoReducer.loading);
-  const error = useSelector((state) => state.cafeInfoReducer.error);
+  const cafeInfo = useSelector((state) => state.cafeInfoReducer);
+  const selectedPost = useSelector((state) => state.cafeInfoReducer.selectedPost);
+  const positiveTag = useSelector((state) => state.cafeInfoReducer.positiveTag);
+  const negativeTag = useSelector((state) => state.cafeInfoReducer.negativeTag);
+  const getHashtagUserId = useSelector(
+    (state) => state.cafeInfoReducer.getHashtagUserId
+  );
   const dispatch = useDispatch();
-
-  const navigate = useNavigate();
-
-  // const search = (num) => dummyData.find((key) => key.id === num);
-  // const cafe = search(Number(id));
-  // const img =
-  //   "https://images.squarespace-cdn.com/content/v1/5bfdafa63917eec8bc387b85/1560564506941-8HTL1GRDEX0UKW0964YT/APC_0050.JPG?format=2500w";
-
-  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     id && dispatch(getCafeInfo(id, isLogin));
-    if (error === true) {
-      alert("세션이 만료되었습니다. 다시 로그인해주세요.");
-      dispatch(login(false));
-      dispatch(showModal(false));
-      dispatch(loginUserInfo(null));
-      dispatch(postCountResetAction());
-      navigate("/");
-    }
   }, [id]);
-
-  // console.log(cafeInfo);
 
   return (
     <>
@@ -59,28 +36,28 @@ const CafeInfo = () => {
           <>
             <S.Cafe1ImageWrapper>
               <Cafe1ImageSection
-                img={cafeInfo.selectedPost.large_img}
-                title={cafeInfo.selectedPost.title}
+                img={selectedPost.large_img}
+                title={selectedPost.title}
               />
             </S.Cafe1ImageWrapper>
             <S.CafePageContainer>
               <S.Cafe2InfoWrapper>
-                <Cafe2InfoSection data={cafeInfo.selectedPost} />
+                <Cafe2InfoSection data={selectedPost} />
               </S.Cafe2InfoWrapper>
 
               <S.Cafe3HashtagWrapper>
                 <Cafe3HashtagSection
-                  positive={cafeInfo.positiveTag}
-                  negative={cafeInfo.negativeTag}
-                  userPick={cafeInfo.getHashtagUserId}
+                  positive={positiveTag}
+                  negative={negativeTag}
+                  userPick={getHashtagUserId}
                 />
               </S.Cafe3HashtagWrapper>
 
               <S.Cafe4MapWrapper>
                 {process.env.REACT_APP_ENV_GOOGLE_MAP === "no" ? null : (
                   <Cafe4MapSection
-                    lat={cafeInfo.selectedPost.lat}
-                    lng={cafeInfo.selectedPost.long}
+                    lat={selectedPost.lat}
+                    lng={selectedPost.long}
                   />
                 )}
               </S.Cafe4MapWrapper>
