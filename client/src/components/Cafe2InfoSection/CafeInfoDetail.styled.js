82a47a856fb2efd42f55fb09f87bfca9ccae7a9c
Tarosism <yar0606@naver.com>
2022-01-17 09:40:41 +0900

    fix: styled컴포 병합

--- client/src/components/Cafe2InfoSection/CafeInfoDetail.styled.js
+++ client/src/components/Cafe2InfoSection/CafeInfoDetail.styled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const CafeInfoContainer = styled.div`
-<<<<<<< HEAD
   background: #50bcdf;
   background: #fdf5e6;
   width: 100%;
@@ -36,49 +35,33 @@ export const Address = styled.span`
   font-weight: 700;
   margin-left: 7px;
   color: black;
-=======
-  background: #fdf5e6;
-  width: 100%;
-  height: 80px;
->>>>>>> d966a3d7763f3ddbc5f899d121555826162ec517
 `;
 
+// export const Phonenumber = styled.div`
+// margin-top :4px;
+// margin-left: 30px;
+// font-size : 22px;
+// opacity: .4;
+// `;
 
+// export const Number = styled.span`
+// font-size : 16px;
+// margin-left : 10px;
+// letter-spacing: .8px;
+// font-weight:700;
+// color: black;
+// `;
 
-export const Phonenumber = styled.div`
-margin-top :4px;
-margin-left: 30px;
-font-size : 22px;
-opacity: .4;
-`;
+// export const Addressd = styled.span`
+// font-size : 22px;
+// margin-left: 30px;
+// letter-spacing: .8px;
+// opacity: .4;
+// `;
 
-
-
-export const Number = styled.span`
-font-size : 16px;
-margin-left : 10px;
-letter-spacing: .8px;
-font-weight:700;
-color: black;
-`;
-
-
-
-export const Addressd = styled.span`
-font-size : 22px;
-margin-left: 30px;
-letter-spacing: .8px;
-opacity: .4;
-`;
-
-
-
-
-
-export const Address = styled.span`
-font-size : 17px;
-font-weight:700;
-margin-left: 7px;
-color: black;
-`;
-
+// export const Address = styled.span`
+// font-size : 17px;
+// font-weight:700;
+// margin-left: 7px;
+// color: black;
+// `;
