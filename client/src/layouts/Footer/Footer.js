565adc576659a3a323b714e8216174158627f8b7
sehyeonpark <psh0866@naver.com>
2022-01-19 14:11:35 +0900

    fix: footer

--- client/src/layouts/Footer/Footer.js
+++ client/src/layouts/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  FaGithub,
+  FaGithub,　FaPhone
 } from 'react-icons/fa';
 
 import * as S from './Footer.styled.js';
@@ -12,24 +12,32 @@ const Footer = () => {
     <S.FooterContainer>
 
       <S.FooterLinksContainer>
+
         <S.FooterLinksWrapper>
-
           <S.FooterLinkItems>
-            <S.FooterLinkTitle>About Us</S.FooterLinkTitle>
-            <S.FooterLink to='/#'>소개</S.FooterLink>
-            <S.FooterLink to='/#'>이용하기</S.FooterLink>
-            <S.FooterLink to='/#'>Contact</S.FooterLink>
+            <S.TeamInfo>Back-end</S.TeamInfo>
+            <S.FooterLink href="https://github.com/kihunism" > <FaGithub /> 문기훈</S.FooterLink>
           </S.FooterLinkItems>
-
         </S.FooterLinksWrapper>
 
         <S.FooterLinksWrapper>
           <S.FooterLinkItems>
-            <S.FooterLinkTitle>Crew</S.FooterLinkTitle>
-            <S.FooterLink to='/#' > <FaGithub /> 문기훈</S.FooterLink>
-            <S.FooterLink to='/#' > <FaGithub /> 양승준</S.FooterLink>
-            <S.FooterLink to='/#' > <FaGithub /> 김윤재</S.FooterLink>
-            <S.FooterLink to='/' > <FaGithub /> 박세현</S.FooterLink>
+            <S.TeamInfo>Back-end</S.TeamInfo>
+            <S.FooterLink href="https://github.com/Tarosism" > <FaGithub /> 양승준</S.FooterLink>
+          </S.FooterLinkItems>
+        </S.FooterLinksWrapper>
+
+        <S.FooterLinksWrapper>
+          <S.FooterLinkItems>
+            <S.TeamInfo>Front-end</S.TeamInfo>
+            <S.FooterLink href="https://github.com/yunjaekim00" > <FaGithub /> 김윤재</S.FooterLink>
+          </S.FooterLinkItems>
+        </S.FooterLinksWrapper>
+
+        <S.FooterLinksWrapper>
+          <S.FooterLinkItems>
+            <S.TeamInfo>Front-end</S.TeamInfo>
+            <S.FooterLink href="https://github.com/park0866" > <FaGithub /> 박세현</S.FooterLink>
           </S.FooterLinkItems>
         </S.FooterLinksWrapper>
 
@@ -40,7 +48,9 @@ const Footer = () => {
           <S.SocialLogo to='/#'>
              <S.SocialIcon /> cafe in
           </S.SocialLogo>
-       <S.WebsiteRights> CafeIn © 2021</S.WebsiteRights>
+          <S.WebsiteRights> CafeIn © 2021</S.WebsiteRights>
+          <S.Contact><FaPhone></FaPhone> 02)1234-1234    
+          <S.Dis>평일 및 토요일 09:00 ~ 18:00 (일요일, 공휴일 제외)</S.Dis> </S.Contact>
         </S.SocialMediaWrap>
       </S.SocialMedia>
 
